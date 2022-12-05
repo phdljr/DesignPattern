cf554954fee3c5b1ce1b559c72379db9928f70c8
phdljr <phdljr@naver.com>
2022-12-05 12:34:17 +0900

    push

---
+++ src/main/java/hf/dp/composite/Menu.java
@@ -0,0 +1,46 @@
+package hf.dp.composite;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Menu extends MenuComponent {
+    ArrayList<MenuComponent> menuComponents = new ArrayList();
+    String name;
+    String description;
+
+    public Menu(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public void add(MenuComponent menuComponent) {
+        menuComponents.add(menuComponent);
+    }
+
+    public void remove(MenuComponent menuComponent) {
+        menuComponents.remove(menuComponent);
+    }
+
+    public MenuComponent getChild(int i) {
+        return (MenuComponent) menuComponents.get(i);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public void print() {
+        Iterator<MenuComponent> iterator = menuComponents.iterator();
+        while (iterator.hasNext()) {
+            MenuComponent menuComponent = iterator.next();
+            menuComponent.print();
+        }
+    }
+}
